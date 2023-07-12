48ea134c1403a672345836a9e38ddacdb5dab477
Oleksandr Yakob <yakoboleksandr@gmail.com>
2023-07-12 15:28:17 +0300

    chore: add routing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,50 +1,13 @@
-import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 import main from "../assets/images/main.svg";
-import styled from "styled-components";
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobsense logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
@@ -57,7 +20,9 @@ const Landing = () => {
             eum qui nesciunt, expedita quas reiciendis, doloremque excepturi
             error, perferendis ipsa eos quod!
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
