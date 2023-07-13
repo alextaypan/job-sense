d315081273608469e155430f3eb25cabca5d93c8
Oleksandr Yakob <yakoboleksandr@gmail.com>
2023-07-13 15:27:25 +0300

    chore: add toggle user

---
+++ src/components/FormRow.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {name}
+      </label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
+};
+export default FormRow;
