fbe8d300093e2486e6375a920c1f4855fd4e4480
Oleksandr Yakob <yakoboleksandr@gmail.com>
2023-07-13 18:21:05 +0300

    chore: add register/login redux initial functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 import { FormRow, Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
 
 const initialState = {
   name: "",
@@ -11,14 +14,27 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
 
+  const dispatch = useDispatch();
   const handleChange = (e) => {
-    console.log(e.target);
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    }
+    if (isMember) {
+      dispatch(loginUser({ email, password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
   };
 
   const toggleMember = () => {
