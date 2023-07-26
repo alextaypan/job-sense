ba2ead400a1c665654fdfdbbcab56b3c36c1f764
Oleksandr Yakob <yakoboleksandr@gmail.com>
2023-07-26 16:52:59 +0300

    chore: add bar&area charts

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import AreaChartComponent from "./AreaChartComponent";
+import BarChartComponent from "./BarChartComponent";
+
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? (
+        <BarChartComponent data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
+  );
 };
 export default ChartsContainer;
